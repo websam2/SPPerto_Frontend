eefb223fb976beafca7c58ad192daf869507f211
Samuel Z <websamrgt@gmail.com>
2024-05-24 13:50:37 -0300

    novos ajustes na tela

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,19 +12,19 @@ interface Link {
 })
 export class AppComponent {
   links: Link[] = [
-    { title: 'Secretaria', links: [1, 2, 3, 4, 5, 6] },
-    { title: 'Secretaria', links: [1, 2, 3, 4, 5, 6] },
-    { title: 'Secretaria', links: [1, 2, 3, 4, 5, 6] },
-    { title: 'Secretaria', links: [1, 2, 3, 4, 5, 6] },
-    { title: 'Secretaria', links: [1, 2, 3, 4, 5, 6] },
-    { title: 'Secretaria', links: [1, 2, 3, 4, 5, 6] },
-    { title: 'Secretaria', links: [1, 2, 3, 4, 5, 6] },
-    { title: 'Secretaria', links: [1, 2, 3, 4, 5, 6] },
-    { title: 'Secretaria', links: [1, 2, 3, 4, 5, 6] },
-    { title: 'Secretaria', links: [1, 2, 3, 4, 5, 6] },
-    { title: 'Secretaria', links: [1, 2, 3, 4, 5, 6] },
-    { title: 'Secretaria', links: [1, 2, 3, 4, 5, 6] },
-    { title: 'Secretaria', links: [1, 2, 3, 4, 5, 6] },
+    { title: 'Nome', links: [1, 2, 3, 4, 5, 6] },
+    { title: 'Nome', links: [1, 2, 3, 4, 5, 6] },
+    { title: 'Nome', links: [1, 2, 3, 4, 5, 6] },
+    { title: 'Nome', links: [1, 2, 3, 4, 5, 6] },
+    { title: 'Nome', links: [1, 2, 3, 4, 5, 6] },
+    { title: 'Nome', links: [1, 2, 3, 4, 5, 6] },
+    { title: 'Nome', links: [1, 2, 3, 4, 5, 6] },
+    { title: 'Nome', links: [1, 2, 3, 4, 5, 6] },
+    { title: 'Nome', links: [1, 2, 3, 4, 5, 6] },
+    { title: 'Nome', links: [1, 2, 3, 4, 5, 6] },
+    { title: 'Nome', links: [1, 2, 3, 4, 5, 6] },
+    { title: 'Nome', links: [1, 2, 3, 4, 5, 6] },
+    { title: 'Nome', links: [1, 2, 3, 4, 5, 6] },
   ];
 //   currentDate: string;
 
