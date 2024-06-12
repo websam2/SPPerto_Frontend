29ba0ce3eac37a60f64b276f884385b959483bed
Samuel Z <websamrgt@gmail.com>
2024-06-12 11:02:13 -0300

    inclusão de data e horas atuais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   secretarias: any[] = [];
-  secretariaFiltrada: any;
 
   constructor(private http: HttpClient) {}
 
@@ -16,44 +15,9 @@ export class AppComponent implements OnInit {
     this.getSecretarias();
   }
 
-  getSecretarias() {
+  getSecretarias(): void {
     this.http.get<any[]>('assets/secretarias.json').subscribe((data) => {
       this.secretarias = data;
-      this.secretariaFiltrada = this.secretarias.find(
-        (secretaria) => secretaria.id === 1
-      );
     });
   }
-
-  getLinks(secretaria: any): string[] {
-    return [
-      secretaria.link_1,
-      secretaria.link_2,
-      secretaria.link_3,
-      secretaria.link_4,
-      secretaria.link_5,
-    ].filter((link) => link);
-  }
 }
-
-// import { Component, OnInit } from '@angular/core';
-// import { ApiService } from './api.service';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css'],
-// })
-// export class AppComponent implements OnInit {
-//   data: any;
-//   errorMessage: string = '';
-
-//   constructor(private apiService: ApiService) {}
-
-//   ngOnInit(): void {
-//     this.apiService.getData().subscribe(
-//       (response) => this.data = response,
-//       (error) => this.errorMessage = error
-//     );
-//   }
-// }
--- src/app/spperto/spperto.component.ts
+++ src/app/spperto/spperto.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment-timezone';
 
 @Component({
   selector: 'app-spperto',
   templateUrl: './spperto.component.html',
   styleUrls: ['./spperto.component.css']
 })
-export class SppertoComponent {
+export class SppertoComponent implements OnInit {
+  currentDateTime: string = '';
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.updateDateTime();
+    setInterval(() => this.updateDateTime(), 1000); // Atualiza a cada segundo
+  }
+
+  updateDateTime(): void {
+    const brasiliaTime = moment().tz('America/Sao_Paulo');
+    this.currentDateTime = brasiliaTime.format('DD/MM/YYYY HH:mm:ss');
+  }
 }
+
